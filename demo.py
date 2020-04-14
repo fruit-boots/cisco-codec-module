3cd33bbcb41490286fd04d76a1c7e5017eba7cde
fruit-boots <adamjbruneau@gmail.com>
2020-04-14 09:37:37 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,7 @@ import ciscocodec
 import pdb
 
 def main():
-    #attributes = {'device_name': 'BOS.Home.Adam.Bruneau', 'region': '', 'ip': '192.168.1.63', 'user': 'demo', 'password': 'BOSvcC1sco!', 'hw_version': '', 'sw_version': 'ce9.12.1.8a845789ad6', 'device_type': 'Cisco Webex DX80', 'sip': '', 'online': True, 'session_cookie': {'Cookie': 'SessionId=2dbbbaaa2423b90894db2bfba4a50d63dc74c058f5d0556d5e8035bfd7fad2b3'}, 'password_verified': True, 'macros_enabled': True, 'macros_autostart': True, 'number_of_macros': 2, 'has_zoom_button': True, 'number_of_panels': 0, 'number_of_extensions': 3, 'connected_devices': 0, 'serial_numbers': {'codec': 'FOC1833NB25'}, 'cdp': {'address': '', 'capabilities': '', 'deviceid': '', 'duplex': '', 'platform': '', 'portid': '', 'primarymgmtaddress': '', 'sysname': '', 'sysobjectid': '', 'vtpmgmtdomain': '', 'version': '', 'voipappliancevlanid': ''}}
-    #codec = ciscocodec_v2.Codec('10.1.81.26', 'admin','BOSvcC1sco!','BOS.fenway')
+    #codec = ciscocodec.Codec('10.1.81.26', 'admin','BOSvcC1sco!','BOS.fenway')
     codec = ciscocodec.Codec('192.168.1.63', 'demo','BOSvcC1sco!','abruneau')
 
     codec.get_session_cookie()
