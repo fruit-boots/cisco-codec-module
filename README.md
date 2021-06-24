a55721ee8fc6a9441f82d429ea7089a9b19ff12f
fruit-boots <adamjbruneau@gmail.com>
2021-06-24 10:39:18 -0400

    updated readme and demo

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Utilizes XMLAPI to get/set certain attributes of a Cisco Codec.
 Module will use Session Cookies. HTTP+HTTPS mode must be set on the codec for it to work.
 Reference demo.py to see the module in action.
 
-`ciscocodec(ip, user, password)` is need to instantiate the class.
+`ciscocodec.Codec(ip, user, password)` is need to instantiate the class.
 
-After instantiation you can import attributes by using `.set_attributes()` method. This will take a dictionary and set the attributes accordingly.
+Make sure to install the requirements shown in requirements.txt. This can be done by running `pip install -r requirements.txt`
 
 See `demo.py` for an example.
--- demo.py
+++ demo.py
@@ -1,58 +1,104 @@
-from ciscocodec import Codec
+import ciscocodec
+import pprint # Used to prettify the attributes to console
 
 """
+You must obtain a cookie from the codec in order to use this module.
+** Cookies are persistent until you close the session or reboot the codec. **
 
-Example of setiing up a codec from scratch. 
+Available commands:
 
-codec = ciscocodec.Codec(ip='192.168.1.63', user='demo',password='BOSvcC1sco!',device_name='abruneau')
+.check_online_status()
+.get_cookie() - mandatory to use any methods below
+.close_session() - always close if you are finished! You can also save this cookie to a file (pickle module works best).
+.get() - see XMLAPI documentation from cisco
+.post() - see XMLAPI documentation from cisco
+.enable_macros() - on/off
+.enable_autostart() - enables macros to start automatically
+.upload_macro()
+.delete_macro()
+.upload_extension()
+.delete_extension()
+.add_user()
+.delete_user()
+.get_codec_details() - retrieves the codecs attibutes mentioned below
+.get_attributes() - returns a dictionary of the objects attributes
+.set_attributes() - only recomended if you absoultely need it. This will override the objects attributes.
 
-Example of importing attributes to an object
-codec.set_attributes({'ip': '192.168.1.63', 'user': 'demo', 'password': 'BOSvcC1sco!', 'device_name': 'BOS.Home.Adam.Bruneau'})
+Object attributes (on instantiation):
+
+        self.ip = ip
+        self.user = user
+        self.password = password
+        
+        # defaults
+        self.online = False
+        self.password_verified = None
+        self.device_name = None
+        self.device_type = None
+        self.sw_version = None
+        self.users = None
+        self.macro_capable = None
+        self.macros_enabled = None
+        self.macros_autostart = None
+        self.macro_names = None
+        self.macro_details = None
+        self.number_of_panels = None
+        self.number_of_extensions = None
+        self.extension_details = None
+        self.session_cookie = None
+        self.configuration_xml = None
+        self.status_xml = None
+        self.timeout = 10
 
 """
 
 """ Gather codec information """
 ip = input('Enter IP: ')
+print('\nWhen entering user credentials ensure they are `Admin` level!')
 user = input('Enter admin username: ')
-password = input('Enter admin password')
-
+password = input('Enter admin password: ')
 codec = ciscocodec.Codec(ip, user, password)
-# get cookie to make calls
-codec.get_cookie()
 
-# make direct calls with get/post
-#codec.get(information)
-#codec.post(command)
+# check online status
+codec.check_online_status()
+
+# get cookie to make calls
+if codec.online:
+    codec.get_cookie()
+
+# make direct calls with get/post if you wish. Reference cisco XMLAPI documentation.
+#codec.get('uri')
+#codec.post('command')
 
 # get codec information
-codec.get_config()
-codec.get_status()
+if codec.session_cookie is not None:
+    print('\nGetting codec information...\n')
+    pprint.pprint(codec.get_codec_details())# make it readable
 
 #-- Set some settings
 #codec.enable_macros('off')
 #codec.enable_macros('on')
 
 #-- Upload/Delete some extensions and macros
-'''
-codec.upload_macro(file, name)
-codec.upload_extension(file)
-print(codec.extension_details)
-print(codec.macro_details)
+
+#codec.upload_macro(file, macro_name)
+#codec.upload_extension(file)
+#print(codec.extension_details)
+#print(codec.macro_details)
 
 #-- Delete macros and extensions
 # delete by name for macro, and by id for extensions
-codec.delete_macro('example'))
-codec.delete_extension(codec.extension_details[0]['id'])
 
-print(codec.extension_details)
-print(codec.macro_details)
-'''
+#codec.delete_macro('example')
+#codec.delete_extension(codec.extension_details[0]['id'])
+
+#print(codec.extension_details)
+#print(codec.macro_details)
 
 #-- Create/Delete user
-'''
-codec.create_user(username, password, role)
-codec.delete_user(username)
-'''
+#codec.create_user(username, password, role)
+#codec.delete_user(username)
 
 # close session
-codec.close_session()
+print('\nClosing session...\n')
+print('Success closing session:',codec.close_session())
